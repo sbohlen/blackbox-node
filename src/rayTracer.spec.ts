ca6168f7c0af00f1cf70b631f16e564a77375506
sbohlen <sbohlen@gmail.com>
2021-09-11 11:25:12 -0400

    initial tests for left/right turns and reflection scenarios

--- src/rayTracer.spec.ts
+++ src/rayTracer.spec.ts
@@ -53,8 +53,8 @@ describe('When traversing a clear path LEFT', () => {
 
 describe('When atom is in path', () => {
   const gameGrid = buildGameGrid(dimensionX, dimensionY, 0);
-
   const pointHavingAtom = new Point(5, 5);
+
   // set the cell to have the atom
   gameGrid.get(pointHavingAtom.toIdString()).hasAtom = true;
 
@@ -67,5 +67,102 @@ describe('When atom is in path', () => {
 
   it('should return hit', () => {
     expect(rayTraceResult.isHit).toEqual(true);
+  });
+});
+
+describe('When atom in upper-left', () => {
+  const gameGrid = buildGameGrid(dimensionX, dimensionY, 0);
+  const pointHavingAtom = new Point(5, 5);
+
+  // set the cell to have the atom
+  gameGrid.get(pointHavingAtom.toIdString()).hasAtom = true;
+
+  const rayEntryPoint = new Point(6, 1);
+
+  const rayTraceResult = traceRay(rayEntryPoint, gameGrid);
+
+  it('should make right turn', () => {
+    expect(rayTraceResult.finalPoint).toEqual(new Point(10, 4));
+  });
+});
+
+describe('When atom in upper-right', () => {
+  const gameGrid = buildGameGrid(dimensionX, dimensionY, 0);
+  const pointHavingAtom = new Point(5, 5);
+
+  // set the cell to have the atom
+  gameGrid.get(pointHavingAtom.toIdString()).hasAtom = true;
+
+  const rayEntryPoint = new Point(4, 1);
+
+  const rayTraceResult = traceRay(rayEntryPoint, gameGrid);
+
+  it('should make left turn', () => {
+    expect(rayTraceResult.finalPoint).toEqual(new Point(1, 4));
+  });
+});
+
+describe('When atom is in upper-left and upper-right', () => {
+  const gameGrid = buildGameGrid(dimensionX, dimensionY, 0);
+
+  const rayEntryPoint = new Point(5, 1);
+
+  // set the cells to have the atoms
+  gameGrid.get(new Point(rayEntryPoint.X - 1, 5).toIdString()).hasAtom = true;
+  gameGrid.get(new Point(rayEntryPoint.X + 1, 5).toIdString()).hasAtom = true;
+
+  const rayTraceResult = traceRay(rayEntryPoint, gameGrid);
+
+  it('should return entry point as final point', () => {
+    expect(rayTraceResult.finalPoint).toEqual(rayEntryPoint);
+  });
+
+  it('should reflect', () => {
+    expect(rayTraceResult.isReflect).toEqual(true);
+  });
+});
+
+describe('When atom is adjacent to entry', () => {
+  const gameGrid = buildGameGrid(dimensionX, dimensionY, 0);
+
+  const rayEntryPoint = new Point(5, 1);
+
+  // set the cell to have the atom
+  gameGrid.get(
+    new Point(rayEntryPoint.X - 1, rayEntryPoint.Y).toIdString(),
+  ).hasAtom = true;
+
+  const rayTraceResult = traceRay(rayEntryPoint, gameGrid);
+
+  it('should return entry point as final point', () => {
+    expect(rayTraceResult.finalPoint).toEqual(rayEntryPoint);
+  });
+
+  it('should reflect', () => {
+    expect(rayTraceResult.isReflect).toEqual(true);
+  });
+});
+
+describe('When two atoms are adjacent to entry', () => {
+  const gameGrid = buildGameGrid(dimensionX, dimensionY, 0);
+
+  const rayEntryPoint = new Point(5, 1);
+
+  // set the cells to have the atoms
+  gameGrid.get(
+    new Point(rayEntryPoint.X - 1, rayEntryPoint.Y).toIdString(),
+  ).hasAtom = true;
+  gameGrid.get(
+    new Point(rayEntryPoint.X + 1, rayEntryPoint.Y).toIdString(),
+  ).hasAtom = true;
+
+  const rayTraceResult = traceRay(rayEntryPoint, gameGrid);
+
+  it('should return entry point as final point', () => {
+    expect(rayTraceResult.finalPoint).toEqual(rayEntryPoint);
+  });
+
+  it('should reflect', () => {
+    expect(rayTraceResult.isReflect).toEqual(true);
   });
 });
