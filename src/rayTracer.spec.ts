631bfe587cc7dcc894efdb6933d1ac84b062bc86
sbohlen <sbohlen@gmail.com>
2021-09-10 22:51:04 -0400

    additional tests to validate proper grid rotation in response to diff. cardinal directions

--- src/rayTracer.spec.ts
+++ src/rayTracer.spec.ts
@@ -2,7 +2,7 @@ import { buildGameGrid } from './buildGameGrid';
 import { Point } from './point';
 import { traceRay } from './rayTracer';
 
-describe('When traversing a clear vertical path', () => {
+describe('When traversing a clear path UP', () => {
   const dimensionX = 10;
   const dimensionY = 10;
 
@@ -13,5 +13,47 @@ describe('When traversing a clear vertical path', () => {
     const rayTraceResult = traceRay(rayEntryPoint, gameGrid);
 
     expect(rayTraceResult.finalPoint).toEqual(new Point(5, 10));
+  });
+});
+
+describe('When traversing a clear path DOWN', () => {
+  const dimensionX = 10;
+  const dimensionY = 10;
+
+  const gameGrid = buildGameGrid(dimensionX, dimensionY, 0);
+  const rayEntryPoint = new Point(5, 10);
+
+  it('should exit on the opposite side of the grid', () => {
+    const rayTraceResult = traceRay(rayEntryPoint, gameGrid);
+
+    expect(rayTraceResult.finalPoint).toEqual(new Point(5, 1));
+  });
+});
+
+describe('When traversing a clear path RIGHT', () => {
+  const dimensionX = 10;
+  const dimensionY = 10;
+
+  const gameGrid = buildGameGrid(dimensionX, dimensionY, 0);
+  const rayEntryPoint = new Point(1, 5);
+
+  it('should exit on the opposite side of the grid', () => {
+    const rayTraceResult = traceRay(rayEntryPoint, gameGrid);
+
+    expect(rayTraceResult.finalPoint).toEqual(new Point(10, 5));
+  });
+});
+
+describe('When traversing a clear path LEFT', () => {
+  const dimensionX = 10;
+  const dimensionY = 10;
+
+  const gameGrid = buildGameGrid(dimensionX, dimensionY, 0);
+  const rayEntryPoint = new Point(10, 5);
+
+  it('should exit on the opposite side of the grid', () => {
+    const rayTraceResult = traceRay(rayEntryPoint, gameGrid);
+
+    expect(rayTraceResult.finalPoint).toEqual(new Point(1, 5));
   });
 });
