51d99d2a1272ed1588e4b178daa67678857400bd
sbohlen <sbohlen@gmail.com>
2021-09-21 21:58:57 -0400

    wire up GameBoard to expose methods that surface update behaviors

--- src/GameBoard.ts
+++ src/GameBoard.ts
@@ -1,13 +1,33 @@
 import { buildGameGrid } from './buildGameGrid';
 import { buildGridAnnotations } from './buildGridAnnotations';
 import { GameGrid } from './GameGrid';
+import { GameStatistics } from './GameStatistics';
 import { GridAnnotations } from './GridAnnotations';
 import { notNullOrUndefined } from './notNullOrUndefined';
+import { Point } from './point';
+import { Direction, traceRay } from './rayTracer';
+import {
+  addGuess,
+  correctGuessCount,
+  incorrectGuessCount,
+  removeGuess,
+  revealAll,
+  updateAnnotation,
+} from './updateGameBoard';
+
+export enum BoardEdge {
+  Top = 'Top',
+  Bottom = 'Bottom',
+  Left = 'Left',
+  Right = 'Right',
+}
 
 export class GameBoard {
   #gameGrid: GameGrid;
 
   #gridAnnotations: GridAnnotations;
+
+  #rayCounter: number;
 
   get GameGrid() {
     return this.#gameGrid;
@@ -30,6 +50,63 @@ export class GameBoard {
 
     this.#gameGrid = buildGameGrid(dimensionX, dimensionY, atomCount);
     this.#gridAnnotations = buildGridAnnotations(dimensionX, dimensionY);
+  }
+
+  sendRay(entryEdge: BoardEdge, cellIndex: number) {
+    let entryPoint: Point;
+    let entryDirection: Direction;
+
+    // translate user-entry to expected params for tracing of the ray
+    switch (entryEdge) {
+      case BoardEdge.Bottom:
+        entryPoint = new Point(cellIndex, this.GameGrid.minY);
+        entryDirection = Direction.Up;
+        break;
+      case BoardEdge.Top:
+        entryPoint = new Point(cellIndex, this.GameGrid.maxY);
+        entryDirection = Direction.Down;
+        break;
+      case BoardEdge.Left:
+        entryPoint = new Point(this.GameGrid.minX, cellIndex);
+        entryDirection = Direction.Right;
+        break;
+      case BoardEdge.Right:
+        entryPoint = new Point(this.GameGrid.maxX, cellIndex);
+        entryDirection = Direction.Left;
+        break;
+      default:
+        throw new Error(`Invalid entryEdge: '${entryEdge}' provided!`); // should NEVER get here
+    }
+
+    // actually do the trace
+    const traceResult = traceRay(entryPoint, this.GameGrid, entryDirection);
+
+    // TODO: update the annotation(s) accordingly
+    updateAnnotation(traceResult, this.GridAnnotations);
+    // increment the counter for later scoring
+    this.#rayCounter += 1;
+  }
+
+  addGuess(x: number, y: number) {
+    addGuess(new Point(x, y), this.GameGrid);
+  }
+
+  removeGuess(x: number, y: number) {
+    removeGuess(new Point(x, y), this.GameGrid);
+  }
+
+  revealAll() {
+    revealAll(this.GameGrid);
+  }
+
+  getGameStatistics(): GameStatistics {
+    const stats = new GameStatistics();
+
+    stats.raysShot = this.#rayCounter;
+    stats.correctGuesses = correctGuessCount(this.GameGrid);
+    stats.incorrectGuesses = incorrectGuessCount(this.GameGrid);
+
+    return stats;
   }
 
   static #validateArgs(
