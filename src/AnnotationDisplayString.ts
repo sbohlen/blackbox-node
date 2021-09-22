4e14bbb9a3571b972ee3367965c630c9ac7d880d
sbohlen <sbohlen@gmail.com>
2021-09-21 21:55:54 -0400

    introduce display string statics for ease of maintenance

---
+++ src/AnnotationDisplayString.ts
@@ -0,0 +1,11 @@
+export class AnnotationDisplayString {
+  static readonly hit = '!';
+
+  static readonly reflectUp = '^';
+
+  static readonly reflectDown = 'v';
+
+  static readonly reflectLeft = '>';
+
+  static readonly reflectRight = '<';
+}
---
+++ src/CellDisplayString.ts
@@ -0,0 +1,11 @@
+export class CellDisplayString {
+  static readonly guess = '?';
+
+  static readonly correctGuess = 'X';
+
+  static readonly incorrectGuess = 'O';
+
+  static readonly missedTarget = 'T';
+
+  static readonly empty = '';
+}
