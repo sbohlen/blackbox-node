20fb4030a918ba2c2e7cc12debdd9fc4c60c7023
sbohlen <sbohlen@gmail.com>
2021-09-29 10:27:09 -0400

    adding DEFLECTED MISS tests and distinguishing from STRAIGHT MISS

--- src/gameplayBehavior.spec.ts
+++ src/gameplayBehavior.spec.ts
@@ -11,7 +11,58 @@ describe.each`
   ${0}        | ${2}        | ${11}      | ${2}       | ${2}           | ${BoardEdge.Left}
   ${11}       | ${2}        | ${0}       | ${2}       | ${2}           | ${BoardEdge.Right}
 `(
-  'When shooting a rays that are a MISS',
+  'When shooting a rays that are a STRAIGHT MISS',
+  ({
+    entryPointX,
+    entryPointY,
+    exitPointX,
+    exitPointY,
+    entryCellIndex,
+    entryBoardEdge,
+  }) => {
+    const dimensionX = 10;
+    const dimensionY = 10;
+    const atomCount = 1;
+
+    const atomPoint = new Point(5, 5);
+
+    const entryPoint = new Point(entryPointX, entryPointY);
+    const exitPoint = new Point(exitPointX, exitPointY);
+
+    const gameBoard = new GameBoard(dimensionX, dimensionY, atomCount);
+
+    utilityClearAllAtomsFromBoard(gameBoard);
+
+    // set single atom at expected location
+    gameBoard.GameGrid.get(atomPoint.toIdString()).hasAtom = true;
+
+    // send the ray that will NOT be influenced by the single atom
+    gameBoard.sendRay(entryBoardEdge, entryCellIndex);
+
+    it('should set entry annotation correctly', () => {
+      expect(
+        gameBoard.GridAnnotations.get(
+          entryPoint.toIdString(),
+        ).toDisplayString(),
+      ).toEqual('A');
+    });
+
+    it('should set exit annotation correctly', () => {
+      expect(
+        gameBoard.GridAnnotations.get(exitPoint.toIdString()).toDisplayString(),
+      ).toEqual('A');
+    });
+  },
+);
+
+describe.each`
+  entryPointX | entryPointY | exitPointX | exitPointY | entryCellIndex | entryBoardEdge
+  ${6}        | ${0}        | ${11}      | ${4}       | ${6}           | ${BoardEdge.Bottom}
+  ${6}        | ${11}       | ${11}      | ${6}       | ${6}           | ${BoardEdge.Top}
+  ${0}        | ${6}        | ${4}       | ${11}      | ${6}           | ${BoardEdge.Left}
+  ${11}       | ${6}        | ${6}       | ${11}      | ${6}           | ${BoardEdge.Right}
+`(
+  'When shooting a rays that are a DEFLECTED MISS',
   ({
     entryPointX,
     entryPointY,
