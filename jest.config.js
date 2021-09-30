018354b4db6ebaa7d0737b788681c879c78bacb3
sbohlen <sbohlen@gmail.com>
2021-09-30 15:13:23 -0400

    config to run balance of tests after first FAIL

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 // eslint-disable-next-line no-undef
 module.exports = {
+  bail: false,
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.+(ts|tsx)', '**/?(*.)+(spec|test).+(ts|tsx)'],
