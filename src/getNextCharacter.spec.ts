fe12dfbe4027076284cf941c64ee3ccb6cf740a8
sbohlen <sbohlen@gmail.com>
2021-09-30 16:48:52 -0400

    enrich test case metadata for reporting

--- src/getNextCharacter.spec.ts
+++ src/getNextCharacter.spec.ts
@@ -1,20 +1,20 @@
 import { getNextLetter as getNextCharacter } from './getNextCharacter';
 
-describe('When selecting next character', () => {
-  it.each`
-    startingCharacter | expectedNextCharacter
-    ${'c'}            | ${'d'}
-    ${'R'}            | ${'S'}
-    ${'z'}            | ${'aa'}
-    ${'Z'}            | ${'AA'}
-    ${'BC'}           | ${'BD'}
-    ${'bc'}           | ${'bd'}
-  `(
-    'should return proper-cased next value',
-    ({ startingCharacter, expectedNextCharacter }) => {
+describe.each`
+  testCaseName                          | startingCharacter | expectedNextCharacter
+  ${'lowercase starting character'}     | ${'c'}            | ${'d'}
+  ${'uppercase starting character'}     | ${'R'}            | ${'S'}
+  ${'lowercase "z" starting character'} | ${'z'}            | ${'aa'}
+  ${'uppercase "Z" starting character'} | ${'Z'}            | ${'AA'}
+  ${'uppercase double-character start'} | ${'BC'}           | ${'BD'}
+  ${'lowercase double-character start'} | ${'bc'}           | ${'bd'}
+`(
+  'When selecting next character',
+  ({ testCaseName, startingCharacter, expectedNextCharacter }) => {
+    it(`${testCaseName} > should return proper-cased next value`, () => {
       const actual = getNextCharacter(startingCharacter);
 
       expect(actual).toEqual(expectedNextCharacter);
-    },
-  );
-});
+    });
+  },
+);
