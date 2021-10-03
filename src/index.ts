38c122ca908d24f357ccda88e65aed783c620469
sbohlen <sbohlen@gmail.com>
2021-10-03 11:01:53 -0400

    refactor into separate function-per-nested-menu paradigm

--- src/index.ts
+++ src/index.ts
@@ -30,35 +30,46 @@ function validateLevelTwoResponse(input: any): boolean | string {
       return `'${input}'' wasn't a good response, try again or 'back' to return`;
   }
 }
+async function levelTwoMenu(): Promise<any> {
+  const levelTwoResponse = await Prompts.prompt({
+    type: 'text',
+    name: 'value',
+    message: "commands: 'hello2'; 'back' to return:",
+    validate: validateLevelTwoResponse,
+  });
+
+  return levelTwoResponse;
+}
+
+async function levelOneMenu(): Promise<any> {
+  const levelOneResponse: any = await Prompts.prompt({
+    type: 'text',
+    name: 'value',
+    message: "commands: 'hello'; 'next' for sub-menu; type 'exit' to end:",
+    validate: validateLevelOneResponse,
+  });
+
+  if (levelOneResponse?.value === 'hello') {
+    console.log('hello from the system!');
+  }
+
+  if (levelOneResponse?.value === 'next') {
+    let levelTwoResponse: any;
+    while (levelTwoResponse?.value !== 'back') {
+      levelTwoResponse = await levelTwoMenu();
+    }
+    // TODO: do we need this?
+    return levelTwoResponse;
+  }
+
+  return levelOneResponse;
+}
 
 (async () => {
   let levelOneResponse: any;
 
   while (levelOneResponse?.value !== 'exit') {
-    levelOneResponse = await Prompts.prompt({
-      type: 'text',
-      name: 'value',
-      message: "commands: 'hello'; 'next' for sub-menu; type 'exit' to end:",
-      validate: validateLevelOneResponse,
-    });
-
-    if (levelOneResponse?.value === 'hello') {
-      console.log('hello from the system!');
-    }
-
-    if (levelOneResponse?.value === 'next') {
-      let levelTwoResponse: any;
-      while (levelTwoResponse?.value !== 'back') {
-        levelTwoResponse = await Prompts.prompt({
-          type: 'text',
-          name: 'value',
-          message: "commands: 'hello2'; 'back' to return:",
-          validate: validateLevelTwoResponse,
-        });
-
-        console.log(levelTwoResponse.value);
-      } // while levelTwoResponse
-    }
+    levelOneResponse = await levelOneMenu();
 
     console.log(levelOneResponse.value);
   } // while levelOneResponse
