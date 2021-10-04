7d870bfa89f54276cda88b35bf1d1faec3a1a2ec
sbohlen <sbohlen@gmail.com>
2021-10-04 16:48:41 -0400

    reconfigure logger to output debug telemetry to file (and IGNORE file)

--- src/logger.ts
+++ src/logger.ts
@@ -2,6 +2,8 @@ import * as winston from 'winston';
 
 export const logger = winston.createLogger({
   level: 'debug',
-  format: winston.format.cli(),
-  transports: [new winston.transports.Console()],
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.File({ filename: 'debug-log.txt', level: 'debug' }),
+  ],
 });
